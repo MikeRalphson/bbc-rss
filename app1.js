290b90c963816a00955fa55f94039bca6218ee8c
Mike Ralphson <mike.ralphson@gmail.com>
2016-03-06 15:15:09 +0000

    modified:   app1.js

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ app.get('/arse', function (req, res) {
   res.send('<html><head><title>Hello</title></head><body><h2>Hello World</h2></body></html>\n');
 });
 
-var myport = 80;
+var myport = 8080;
 if (process.argv.length>2) myport = process.argv[2];
 
 var server = app.listen(myport, function () {
