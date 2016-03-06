4caa82f563a82a213bc7dcf46f5e9994950eba05
Mike Ralphson <mike.ralphson@gmail.com>
2016-03-06 17:08:26 +0000

    modified:   app1.js

--- app1.js
+++ app1.js
@@ -5,6 +5,10 @@ app.get('/arse', function (req, res) {
   res.send('<html><head><title>Hello</title></head><body><h2>Hello World</h2></body></html>\n');
 });
 
+app.get('/nitro/*', function (req, res) {
+  res.send('<html><head><title>Nitro Proxy</title></head><body><h2>Log content here</h2></body></html>\n');
+});
+
 var myport = process.env.PORT || 3000;
 if (process.argv.length>2) myport = process.argv[2];
 
