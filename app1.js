a964b43acafd41225a6946256c7fe06d4e77a18f
Mike Ralphson <mike.ralphson@gmail.com>
2016-03-06 14:56:27 +0000

    Initial commit

---
+++ app1.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/arse', function (req, res) {
+  res.send('<html><head><title>Hello</title></head><body><h2>Hello World</h2></body></html>\n');
+});
+
+var myport = 80;
+if (process.argv.length>2) myport = process.argv[2];
+
+var server = app.listen(myport, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
